0c77fa3356f03754e67d491440ae288b9f3f1c5d
Hubert Niewiński <hubertniewinski@yahoo.com>
2021-11-24 23:39:33 +0100

    seed db data

--- Entities/RestaurantDbContext.cs
+++ Entities/RestaurantDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace RestaurantAPI.Entities
 {
@@ -30,6 +31,67 @@ namespace RestaurantAPI.Entities
               .Property(r => r.Street)
               .IsRequired()
               .HasMaxLength(50);
+
+            modelBuilder.Entity<Restaurant>()
+                .HasData(
+                new ()
+                {
+                    Id = 1,
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description =
+                        "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquartered in Louisville, Kentucky, that specializes in fried chicken.",
+                    ContactEmail = "contact@kfc.com",
+                    HasDelivery = true,
+                    AddressId = 1
+                },
+                new ()
+                {
+                    Id = 2,
+                    Name = "McDonald Szewska",
+                    Category = "Fast Food",
+                    Description =
+                        "McDonald's Corporation (McDonald's), incorporated on December 21, 1964, operates and franchises McDonald's restaurants.",
+                    ContactEmail = "contact@mcdonald.com",
+                    HasDelivery = true,
+                    AddressId = 2
+                });
+
+            modelBuilder.Entity<Dish>()
+                .HasData(
+                    new ()
+                    {
+                        Id = 1,
+                        Name = "Nashville Hot Chicken",
+                        Price = 10.30M,
+                        RestaurantId = 1
+                    },
+                    new ()
+                    {
+                        Id = 2,
+                        Name = "Chicken Nuggets",
+                        Price = 5.30M,
+                        RestaurantId = 1
+                    }
+                );
+
+            modelBuilder.Entity<Address>()
+                .HasData(
+                     new ()
+                     {
+                         Id = 1,
+                         City = "Kraków",
+                         Street = "Długa 5",
+                         PostalCode = "30-001"
+                     },
+                    new ()
+                    {
+                        Id = 2,
+                        City = "Kraków",
+                        Street = "Szewska 2",
+                        PostalCode = "30-001"
+                    }
+                );
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
